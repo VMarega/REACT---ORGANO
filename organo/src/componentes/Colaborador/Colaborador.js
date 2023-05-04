41861b973bff4b2db994fc63ba2977fdd1035ce3
VMarega <vinimarega@gmail.com>
2023-05-04 16:43:31 -0300

    aula 4

---
+++ organo/src/componentes/Colaborador/Colaborador.js
@@ -0,0 +1,20 @@
+import './Colaborador.css'
+
+const Colaborador = ()=>{
+    return(
+    <div>
+        <div>
+            <img src='http://github.com/VMarega.png' alt='Vinicius Marega'></img>
+        </div>
+        <div>
+            <h4>
+                Vinicius
+            </h4>
+            <h5>
+                Aluno
+            </h5>
+        </div>
+    </div>)
+}
+
+export default Colaborador
